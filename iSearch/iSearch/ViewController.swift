cb126adca3067e697fddaaf974d7bcf455a39557
Franco Leto <letoefranco@gmail.com>
2020-02-18 19:43:37 -0300

    Create iSearch Project

---
+++ iSearch/iSearch/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iSearch
+//
+//  Created by Franco Leto on 18/02/2020.
+//  Copyright © 2020 FrancoLeto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
